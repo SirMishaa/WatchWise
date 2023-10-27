19da31c7678b56f44b287bc9dc8a354b3309703e
Mishaa <mishaa.pro@proton.me>
2023-10-27 04:36:23 +0200

    :sparkles: add tracing and tower_http middleware to log things

--- watchwise-server/src/main.rs
+++ watchwise-server/src/main.rs
@@ -2,12 +2,26 @@ use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::Router;
 use std::net;
+use tower_http::trace;
+use tower_http::trace::TraceLayer;
+use tracing::{info, Level};
 
 static SERVER_ADDRESS: &str = "127.0.0.1:3005";
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handle));
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
+
+    let mut app = Router::new().route("/", get(handle));
+
+    app = app.layer(
+        TraceLayer::new_for_http()
+            .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+            .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+    );
 
     let listener = net::TcpListener::bind(SERVER_ADDRESS).unwrap_or_else(|_| {
         panic!(
@@ -16,7 +30,7 @@ async fn main() {
         )
     });
 
-    println!("Application started to listening on {}", SERVER_ADDRESS);
+    info!("Application started to listening on {}", SERVER_ADDRESS);
     axum::Server::from_tcp(listener)
         .expect("Unable to bind server to listener")
         .serve(app.into_make_service())
